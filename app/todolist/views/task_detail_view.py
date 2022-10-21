e82a10fc0d9459e9b33e007569f3dcee3a6b9720
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2022-10-21 07:12:15 +0600

    Убрала метод get_context_data и оставила только get_queryset и удаленные задачи в детальном просмотре задач не выходит а ошибку выдает

--- app/todolist/views/task_detail_view.py
+++ app/todolist/views/task_detail_view.py
@@ -8,11 +8,6 @@ class TaskDetailView(DetailView):
     model = Tasks
     context_object_name = 'todo_task'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['todo_task'] = get_object_or_404(Tasks, pk=kwargs['pk'])
-        return context
-
     def get_queryset(self):
         queryset = super().get_queryset().exclude(is_deleted=True)
         return queryset
