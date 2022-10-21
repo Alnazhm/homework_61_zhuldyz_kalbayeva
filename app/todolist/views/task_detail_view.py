c30c6f88d9d7ae3191c431a752c0404c5d3744f6
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2022-10-21 06:00:35 +0600

    Доработала замечания, если задач нет выводится прямо текст, а не 404 Error и теперь не будет отображаться удаленная задача

--- app/todolist/views/task_detail_view.py
+++ app/todolist/views/task_detail_view.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from todolist.models import Tasks
 from django.views.generic import DetailView
 
@@ -6,3 +7,8 @@ class TaskDetailView(DetailView):
     template_name = 'task.html'
     model = Tasks
     context_object_name = 'todo_task'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Tasks, pk=kwargs['pk'])
+        return context
